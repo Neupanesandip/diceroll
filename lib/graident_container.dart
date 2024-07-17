ae2052218df76caf10fac36a07701bab92e3bbea
Sandip <neupsandip336@gmail.com>
2024-07-17 09:48:50 +0545

    first commit

---
+++ lib/graident_container.dart
@@ -0,0 +1,26 @@
+import 'package:dice_roling/dice_roller.dart';
+import 'package:flutter/material.dart';
+
+var startAlignment = Alignment.topLeft;
+var endAlignment = Alignment.bottomRight;
+
+class GradientContainer extends StatelessWidget {
+  const GradientContainer(this.color1, this.color2, {super.key});
+  final Color color1;
+  final Color color2;
+  @override
+  Widget build(context) {
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: [color1, color2],
+          begin: startAlignment,
+          end: endAlignment,
+        ),
+      ),
+      child: Center(
+        child: DiceRoller(),
+      ),
+    );
+  }
+}
